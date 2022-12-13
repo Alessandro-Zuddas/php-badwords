3a6a1f38903b3d4cc23284f1fa39bc870b43ea25
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-12-13 15:08:24 +0100

    Agg. replace badwords con $_GET e stamp su pagina

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-    $all_text = "PHP è un linguaggio di scripting generico open source ampiamente utilizzato, particolarmente adatto per lo sviluppo web e può essere incorporato in HTML. Invece di molti comandi per l'output HTML, le pagine PHP contengono HTML con codice incorporato che fa 'qualcosa'. Il codice PHP è racchiuso in speciali istruzioni di inizio e fine elaborazione <?phpe?> che ti consentono di entrare e uscire dalla `modalità PHP`."
-    
+    $all_text = "PHP è un linguaggio di scripting generico open source ampiamente utilizzato, particolarmente adatto per lo sviluppo web e può essere incorporato in HTML. Invece di molti comandi per l'output HTML, le pagine PHP contengono HTML con codice incorporato che fa 'qualcosa'. Il codice PHP è racchiuso in speciali istruzioni di inizio e fine elaborazione <?phpe?> che ti consentono di entrare e uscire dalla `modalità PHP`.";
+    $censored_text = str_replace($_GET["bad_word"], "***", $all_text);
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +19,11 @@
                 <h1>Frase non censurata:</h1>
                 <p> <?php echo $all_text ?> </p>
                 <strong> Lunghezza frase: <?php echo strlen($all_text) ?> </strong>
+            </div>
+            <div class="container-two">
+                <h1>Frase censurata:</h1>
+                <p> <?php echo $censored_text ?> </p>
+                <strong> Lunghezza frase: <?php echo strlen($censored_text) ?> </strong>
             </div>
         </div>
 
